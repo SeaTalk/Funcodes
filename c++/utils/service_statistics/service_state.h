bcfb6abd7a75d9b3d4aaa14456579625c74c6a00
SeaTalk <myway2@sina.cn>
2018-10-17 23:54:23 +0800

    add service statistics

--- c++/utils/service_statistics/service_state.h
+++ c++/utils/service_statistics/service_state.h
@@ -2,12 +2,13 @@
 #define I_SERVICE_STATE_H
 
 template <typename T>
-class i_state{
+class i_state
+{
 
 public:
     virtual ~i_state(){}
     virtual void init_state() = 0;
-    virtual void update_state() = 0;
+    virtual void update_state(T) = 0;
     virtual T get_state() = 0;
 
 };
--- c++/utils/service_statistics/sliding_window.h
+++ c++/utils/service_statistics/sliding_window.h
@@ -6,8 +6,8 @@
 template <typename I>
 struct state_info
 {
-unsigned int64_t timestamp;
-I state_value;
+    uint64_t timestamp;
+    I state_value;
 };
 
 template <typename S>
@@ -16,19 +16,21 @@ class sliding_window_state
 {
 
 private:
-    unsigned int32_t m_window_size;
+    unsigned int32_t m_window_capacity;
     struct state_info<S> *m_fixsized_slots;
-    int m_front;
-    int m_rear;
+    int32_t m_front;
+    int32_t m_rear;
+    int32_t m_window_size;
 public:
     sliding_window_state(unsigned int32_t size):
-        m_window_size(size),
+        m_window_capacity(size),
         m_fixsized_slots(NULL),
         m_front(0),
-        m_rear(0)
+        m_rear(0),
+        m_window_size(0);
     {
         m_fixsized_slots = new state_info[size+1];
-        m_front = m_rear = m_fixsized_slots;
+        m_front = m_rear = 0;
     }
     
     ~sliding_window_state()
@@ -37,36 +39,10 @@ public:
         m_fixsized_slots =  NULL:
     }
 
-    bool isEmpty()
-    {
-        return m_front == m_rear;
-    }
-
-    bool isFull()
-    {
-        return m_front == (m_rear + 1)%m_window_size;
-    }
-
-    int push(state_info<S> &item)
-    {
-        if(isFull())
-        {
-            return -1;
-        }
-        m_fixsized_slots[m_rear] = item;
-        m_rear = (m_rear + 1)%m_window_size;
-    }
-
-    state_info<S> pop()
-    {
-        if(isEmpty())
-        {
-            return -1;
-        }
-        state_info<S> tmp = m_fixed_slots[m_front];
-        m_front = (m_front + 1) % m_window_size;
-        return tmp;
-    }
+    bool isEmpty();
+    bool isFull();
+    int push(state_info<S> &item);
+    state_info<S> pop();
 }; 
 
 template <typename T>
